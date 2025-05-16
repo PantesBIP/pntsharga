62095b84374795ede7bb31fc90a21e8de1b5cea5
PantesBIP <pantesbandungindahplaza@gmail.com>
2025-05-16 20:25:56 +0700

    Update script.js

--- a/test/script.js
+++ a/test/script.js
@@ -1,5 +1,5 @@
 // Configurations
-const SPREADSHEET_BASE = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQFH0squhL_c2KoNryfBrysWZEKTTUpthg_1XVE-fT3r7-ew1_lkbFqENefrlBLHClis53FyDdNiUkh/pub?gid=0&single=true&output=csv';
+const SPREADSHEET_BASE = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQFH0squhL_c2KoNryfBrysWZEKTTUpthg_1XVE-fT3r7-ew1_lkbFqENefrlBLHClis53FyDdNiUkh/pub?output=csv';
 
 const SHEET_CONFIG = {
   harga: { gid: '216173443', name: 'Harga' },
